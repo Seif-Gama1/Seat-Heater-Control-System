3e2d15c495f62067f01774e86f92282bdabac66b
Seif-Gama1 <126584186+Seif-Gama1@users.noreply.github.com>
2024-04-27 15:34:23 +0300

    Update adc.h

--- FreeRTOS_Project/MCAL/ADC/adc.h
+++ FreeRTOS_Project/MCAL/ADC/adc.h
@@ -5,6 +5,9 @@
 #include "std_types.h"
 #include "gpio.h"
 
+#define NVIC_EN0_R                (*((volatile uint32 *)0xE000E100))
+#define NVIC_EN1_R                (*((volatile uint32 *)0xE000E104))
+
 /*ADC0 module*/
 #define ADC0_ADCACTSS (*((volatile unsigned long*)0x40038000))
 #define ADC0_ADCEMUX (*((volatile unsigned long*)0x40038014))
@@ -16,7 +19,6 @@
 #define ADC0_ADCSSFIFO3 (*((volatile unsigned long*)0x400380A8))
 #define ADC0_ADCRIS (*((volatile unsigned long*)0x40038004))
 #define ADC0_ADCIM  (*((volatile unsigned long*)0x40038008))
-#define ADC0_ADCISC (*((volatile unsigned long*)0x40038000))
 
 /*******************************************************************************************************************/
 /*ADC1 module*/
@@ -29,39 +31,16 @@
 #define ADC1_ADCSSFIFO3 (*((volatile unsigned long*)0x400390A8))
 #define ADC1_ADCRIS (*((volatile unsigned long*)0x40039004))
 #define ADC1_ADCIM  (*((volatile unsigned long*)0x40039008))
-#define ADC1_ADCISC (*((volatile unsigned long*)0x40039000))
 
 /*******************************************************************************************************************/
-
-
-/*
-typedef enum{
-    PE3=0,
-    PE2,
-    PE1,
-    PE0,
-    PD3,
-    PD2,
-    PD1,
-    PD0,
-    PE5,
-    PE4,
-    PB4,
-    PB5
-
-}ADC_PIN_ID;
-
-typedef enum{
-    ADC0=0,
-    ADC1
-
-}ADC_ID;
-*/
 
 
 void ADC_Init(void);
 void ADC_SampleSeqInit(void);
 void ADC_ModuleInit(void);
+
+uint32 ADC0_readChannel (void);
+uint32 ADC1_readChannel (void);
 
 /* u32 ADC_Get_Data(ADC_PIN_ID PIN); */
 
